6025d2bdfd025bca12afc2f3f124dc796326144b
Minsu <m.kim2827@gmail.com>
2024-04-14 02:59:34 +1200

    worflow-properly-working

--- frontend/Tome/vite.config.js
+++ frontend/Tome/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<repository>/',
+  base: '/Tome/',
   plugins: [react()],
   resolve: {
     alias: {
