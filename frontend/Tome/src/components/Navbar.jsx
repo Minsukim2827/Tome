514fa78a1f561c756167b5ce9b86562de09b8477
Minsu <m.kim2827@gmail.com>
2024-04-15 15:02:17 +1200

    v1

--- frontend/Tome/src/components/Navbar.jsx
+++ frontend/Tome/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BookOpenText, Menu, X } from 'lucide-react';
-import '@/css/index.css'; // Import Tailwind CSS
+import '@/css/index.css'; // Import TailwindCSS
 
 const navItems = [
   { label: 'Home', href: '/' },
